a08cb6c8503860220198cffffc4ce566a974ffa1
Konynunez <konynunez@gmail.com>
2024-10-08 01:02:38 -0400

    vercel.json edited

--- supabaseInstance.js
+++ supabaseInstance.js
@@ -1,22 +1,15 @@
+"checkJS";
+// Import Dotenv
 require("dotenv").config();
 const axios = require("axios");
 
 const instance = axios.create({
   baseURL: process.env.SUPABASE_URL + "/rest/v1",
-  timeout: 5000, // Increased timeout for larger or delayed responses
+  timeout: 3000,
   headers: {
     apikey: process.env.SUPABASE_KEY,
     Authorization: "Bearer " + process.env.SUPABASE_KEY,
   },
 });
-
-// Optional: Add interceptors for better error handling
-instance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.error("API request error:", error.message);
-    return Promise.reject(error);
-  }
-);
 
 module.exports = instance;
